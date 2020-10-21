7e6744a83bdadce2d21cf8f643309457072097de
dimitri20 <61322404+dimitriX20@users.noreply.github.com>
2020-10-21 18:29:29 +0200

    Create Template.cpp

---
+++ Template.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ar array
+#define ll long long
+
+const int MOD = 1e9 + 7;
+const int INF = 1e9;
+
+
+void solve() {
+
+}
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    freopen("input.txt", "r", stdin); // redirects standard input
+    freopen("output.txt", "w", stdout); // redirects standard output
+
+    int tc; cin >> tc;
+    for (int t = 1; t <= tc; t++) {
+        // cout << "Case #" << t  << ": ";
+        solve();
+    }
+}
